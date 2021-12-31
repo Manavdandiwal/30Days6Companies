0acf4fa3acdbe12837ee3f716350286bf7313c45
Manavdandiwal <73937633+Manavdandiwal@users.noreply.github.com>
2021-12-31 17:34:40 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,16 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define all(x) x.begin(), x.end()
-#define _sort(x) sort(all(x))
-#define __sort(x, y) sort(all(x), y)
-#define vi vector<int>
-#define vl vector<ll>
-#define loop(i, N) for (int i = 0; i < N; i++)
-#define _loop(i, x, N) for (int i = x; i < N; i++)
-#define __loop(i, x, N, y) for (int i = x; i < N; i += y)
-#define mod 1000000007
-
 class Solution{
   public:
     vector<vector<string> > Anagrams(vector<string>& string_list) 
@@ -33,27 +20,3 @@ class Solution{
         return ans;
     }
 };
-
-int main()
-{
-    int T;
-    cin >> T;
-
-    while (T--)
-    {
-        int N;
-        cin >> N;
-
-        vi arr(N);
-
-        for (int i = 0; i < N; i++)
-            cin >> arr[i];
-
-        vi count(100, 0);
-
-        for (int i = 0; i < N; i++)
-        {
-            count[abs(arr[i])]++;
-        }
-    }
-}
